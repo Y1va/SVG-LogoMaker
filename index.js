36186fea490618b9ba7fc8eb9f7cd57a33d02862
ZeuS <ponzeadrian@gmail.com>
2024-02-04 12:15:20 +1100

    debugged constructors

--- index.js
+++ index.js
@@ -1,73 +1,56 @@
-// Runs the application using imports from lib
-
-const inquirer = require("inquirer");
-const fs = require("fs");
-// Destructuring with {}
-const {Circle, Triangle, Square} = require("./lib/shapes");
-
-
-
-// Only let users enter 3 characters or less for their logo text
-
-const validateInput = function(name) {
-  if (name.length > 3) {
-    return "Please enter up to 3 characters only";
-  }
-  return true;
-}
-
+// PULL IN DEPENDANCIES AND SHAPES FILE
+const inquirer = require('inquirer');
+const { Circle, Triangle, Square } = require('./lib/shapes.js');
+const fs = require('fs');
+// VALIDATION FOR USERS TO ENTER 3 OR LESS CHARS FOR LOGO TEXT
+const validateInput = (name) => {
+    if (name.length > 3) {
+      return 'Please enter up to 3 characters only.';
+    }
+    return true;
+};
+// USER INPUT/SELECTION
 inquirer.prompt([
-  {
-    message: "Please enter up to 3 characters",
-    name: "characters",
-    validate: validateInput
-  },
-  {
-    message: "Please enter the text colour (Name or hexidecimal)",
-    name: "text-color"
-  },
-  {
-    message: "Please choose logo shape",
-    type: "list",
-    choices: [
-      "Circle",
-      "Triangle",
-      "Square"
-    ],
-    name: "shape"
-  },
-  {
-    message: "Please enter the shape colour (Name or hexidecimal)",
-    name: "shape-color"
-  }
+    {
+        message: 'Please enter up to 3 characters:',
+        name: 'characters',
+        validate: validateInput
+    },
+    {
+        message: 'Please enter the text color (name or hexadecimal):',
+        name: 'text-color'
+    },
+    {
+        type: 'list',
+        message: 'Please choose logo shape:',
+        choices: ['Circle', 'Triangle', 'Square'],
+        name: 'shape'
+    },
+    {
+        message: 'Please enter the shape color (name or hexadecimal):',
+        name: 'shape-color'
+    }
 ]).then((answers) => {
-  console.log("Generating Logo.svg")
-  const shapeColor = answers["shape-color"];
-  const textColor = answers["text-color"];
-  const textContent = answers["characters"];
-
-  let shape;
-  if (answers.shape === Circle) {
-    shape = new Circle(shapeColor);
-  }
-  else if (answers.shape === Triangle) {
-    shape = new Triangle(shapeColor);
-  }
-  else if (answers.shape === Square) {
-    shape = new Square(shapeColor);
-  }
-
-
-  // Generate SVG with color and text
-  const svgLogo = shape.draw({color: textColor, content: textContent})
-
-
-  // Save SVG to a file
-  fs.writeFile("logo.svg", svgLogo, (err) => {
-    if (err) {
-      console.log("Failed to save SVG");
-    } else {
-      console.log("SVG saved successfully");
-    } 
-  })
+    console.log('Generated logo.svg');
+    const shapeColor = answers['shape-color'];
+    const textColor = answers['text-color'];
+    const textContent = answers['characters'];
+    let shape;
+    if (answers.shape === 'Circle') {
+        shape = new Circle(shapeColor);
+    } else if (answers.shape === 'Triangle') {
+        shape = new Triangle(shapeColor);
+    } else if (answers.shape === 'Square') {
+        shape = new Square(shapeColor);
+    }
+    // GENERATE SVG WITH INPUTED COLOR AND TEXT
+    const svgLogo = shape.draw({ color: textColor, content: textContent });
+    // DAVING SVG TO FILE
+    fs.writeFile('logo.svg', svgLogo, (err) => {
+        if (err) {
+            console.error('Failed to save SVG:', err);
+        } else {
+            console.log('SVG logo saved to logo.svg');
+        }
+    });
 });
